2ad8625d8bdc7111e66a8566f1186a8fac670647
matsu <tmtmt2209@gmail.com>
2024-01-05 14:48:57 +0000

    refactor: relocate component trait

--- src/app/components.rs
+++ src/app/components.rs
@@ -5,3 +5,10 @@ mod wait;
 pub use cmd_executor::*;
 pub use cmd_not_found::*;
 pub use wait::*;
+
+#[async_trait::async_trait]
+pub trait Component {
+    type Output;
+
+    async fn handle(&self) -> Self::Output;
+}
